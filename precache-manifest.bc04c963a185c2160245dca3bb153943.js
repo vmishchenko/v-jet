52c150d681126f4281ecd4add1bd3f75a621f1b7
vmishchenko <vladislav.a.mishchenko@gmail.com>
2020-01-16 00:51:35 +0200

    Updates

---
+++ precache-manifest.bc04c963a185c2160245dca3bb153943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c710c780f8f0f446d9b954e2733b109d",
+    "url": "/v-jet/index.html"
+  },
+  {
+    "revision": "d4970238169353e6d855",
+    "url": "/v-jet/static/css/main.ece3561d.chunk.css"
+  },
+  {
+    "revision": "ab0f357db4c2889055c3",
+    "url": "/v-jet/static/js/2.e01bc869.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/v-jet/static/js/2.e01bc869.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4970238169353e6d855",
+    "url": "/v-jet/static/js/main.dacdc965.chunk.js"
+  },
+  {
+    "revision": "10fbde7cd53ea5635cfc",
+    "url": "/v-jet/static/js/runtime-main.4552305a.js"
+  }
+]);
